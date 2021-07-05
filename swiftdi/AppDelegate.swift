523e2fbe06269d69a4481417e93b0fb1b3c3f67d
남오승 <developernam92@gmail.com>
2021-07-05 10:23:03 +0900

    수정(appDelegate) * dependency 등록작업 수정

--- swiftdi/AppDelegate.swift
+++ swiftdi/AppDelegate.swift
@@ -6,45 +6,38 @@
 //
 
 import UIKit
-import Swinject
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    let container: Container = {
-        let container = Container()
-        container.register(Animal.self) { _ in Cat(name: "Mini")}
-        container.register(Animal.self) { _ in Dog(name: "OSeung")}
-        
-        
-        container.register(ViewController.self) { resolver in
-            let controller = ViewController()
-            controller.animal = resolver.resolve(Animal.self)
-            return controller
-        }
-        
-        return container
-    }()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        registerDependencies()
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    //디펜던시 등록작업
+    private func registerDependencies() {
+        DIContainer.shared.register(UserInfo())
+        
+        let userInfo: UserInfo = DIContainer.shared.resolve()
+        
+        DIContainer.shared.register(userInfo)
+        
+    }
 }
 
