7c2276e3d9e0b7f144944f1dc922ac7c4409e872
남오승 <developernam92@gmail.com>
2021-07-05 10:22:03 +0900

    신규 * DI 테스트용 객체 추가

---
+++ swiftdi/UserInfo.swift
@@ -0,0 +1,23 @@
+//
+//  UserInfo.swift
+//  swiftdi
+//
+//  Created by OSeung Nam on 2021/07/05.
+//
+
+import Foundation
+
+
+class UserInfo {
+    var userName:String = {
+        return "NAM"
+    }()
+    
+    var userEmail: String = {
+        return "developernam92@gmail.com"
+    }()
+    
+    var userAddress: String = {
+        return "Seoul"
+    }()
+}
